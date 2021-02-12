a4406972bc56fcbc32600640414c9cc318f82db1
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-02-12 20:16:56 +0300

    project created

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-hw4-t2-p1';
+
+  date:number = Date.now();
+}
